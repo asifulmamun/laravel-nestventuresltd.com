b3bbf5a7dc4e081296f697b094025b7ac0315e69
asifulmamun <asifulmamun@gmail.com>
2024-06-01 19:09:29 +0600

    cmpser

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '87879980634f59161f2d56ee6a1eb1b91f70d406',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -389,9 +389,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '87879980634f59161f2d56ee6a1eb1b91f70d406',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
